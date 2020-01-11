4a2ccaa9c422ee7239c1bcc07396a4008b0b79a4
Enric <enriccamachora@gmail.com>
2020-01-11 18:19:36 +0100

    added logo para ventana web

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        //icon: `src/static/img/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/static/img/logoweb.png`, // This path is relative to the root of the site.
       },
     },
     {
